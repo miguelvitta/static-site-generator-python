a5ea19a8c239497ac32941b8cd028a02b98e5d85
Miguel Vitta <miguelvittadev@gmail.com>
2026-03-11 11:10:47 -0300

    feat: adds the basic paragraph splitter function and test

---
+++ src/markdown_blocks.py
@@ -0,0 +1,8 @@
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    cleaned = []
+    for block in blocks:
+        block = block.strip()
+        if block != "":
+            cleaned.append(block)
+    return cleaned
